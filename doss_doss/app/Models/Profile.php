2fc270be959497a234b000c2898b1acd3b6c1e2a
majdba123 <137396563+majdba123@users.noreply.github.com>
2025-06-18 15:49:27 +0300

    Update Profile.php

--- doss_doss/app/Models/Profile.php
+++ doss_doss/app/Models/Profile.php
@@ -5,25 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Profile extends Model
+class profile extends Model
 {
     use HasFactory;
-        protected $fillable = [
-        'user_id',
+    protected $fillable = [
         'first_name',
         'last_name',
+        'user_id',
         'address_1',
         'address_2',
         'zip_code',
         'city',
+        'image',
     ];
-
-
-
-    public function user()
-    {
-        return $this->belongsTo(User::class , "user_id");
-    }
-
-
 }
