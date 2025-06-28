bab69774fe5c98c9268dcf5481282e5bb4542d7b
majdba123 <137396563+majdba123@users.noreply.github.com>
2025-06-28 13:58:21 +0300

    Revert "12"
    This reverts commit 58e87ccee01210e8e740c24d4290e8ef2e3e530d.

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\DriverPriceController;
 use App\Http\Controllers\UserNotifyController;
-use App\Http\Controllers\DeepSeekController;
 
 /*
 |--------------------------------------------------------------------------
@@ -228,6 +227,3 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('auth/facebook/callback', [FacebookController::class, 'callback']);
 });
 */
-
-
-Route::post('/deepseek-chat', [DeepSeekController::class, 'chat'])->middleware('throttle:10,1');;
